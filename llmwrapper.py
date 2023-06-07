c33f533f07de7b40f0643ce48da0fda761498f1b
Rein <rein@catatonic.ai>
2023-06-07 13:29:34 +0300

    save

--- llmwrapper.py
+++ llmwrapper.py
@@ -1,23 +1,60 @@
 
 
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    Dict,
+    List,
+    Mapping,
+    Optional,
+    Tuple,
+    Union,
+)
+
+from pydantic import Extra, Field, root_validator
+
 import requests
 import json
 
-from typing import Any, List, Mapping, Optional
 
 from langchain.callbacks.manager import CallbackManagerForLLMRun
+from langchain.chat_models.base import BaseChatModel
 from langchain.llms.base import LLM
+from langchain.schema import Generation, LLMResult
+from langchain.callbacks.manager import (
+    AsyncCallbackManager,
+    AsyncCallbackManagerForLLMRun,
+    CallbackManager,
+    CallbackManagerForLLMRun,
+    Callbacks
+)
+from langchain.schema import (
+    AIMessage,
+    BaseMessage,
+    ChatGeneration,
+    ChatMessage,
+    ChatResult,
+    HumanMessage,
+    SystemMessage
+)
 
 
 
+
+
+
+
+
+# a langchain LLM just handles text - you probaby want a chatmodel instead
 class CustomLLM(LLM):
 
-    service: str
-    model: str
+    service_name: str
+    model_name: str
 
     @property
     def _llm_type(self) -> str:
-        return "custom"
+        return self.service_name + "." + self.model_name
 
     def _call(
         self,
@@ -31,8 +68,8 @@ class CustomLLM(LLM):
         response = requests.post(
             url="http://localhost:3010/llm",
             json={
-                "service": self.service,
-                "model":  self.model,
+                "service": self.service_name,
+                "model":  self.model_name,
                 "prompt": prompt
             },
         )
@@ -64,18 +101,65 @@ class CustomLLM(LLM):
 
 
 
-llm = CustomLLM(service="openai", model="gpt-4")
+def testLLM():
 
-result = llm(
-'''In less than 10 words, answer the following questions:
-Do you know the way to San Jose?''')
+    llm = CustomLLM(service_name="openai", model_name="gpt-4")
+    result = llm(
+    '''In less than 10 words, answer the following questions:
+    Do you know the way to San Jose?''')
+    print(result)
 
-print(result)
+    #result = llm("You can really breathe in San Jose")
+    #print(result)
 
-result = llm("You can really breathe in San Jose")
+    #result = llm("I got lots of friends in San Jose")
+    #print(result)
 
-print(result)
 
-result = llm("I got lots of friends in San Jose")
 
-print(result)
+class CustomChatModel(BaseChatModel):
+    service_name: str
+    model_name: str
+
+    @property
+    def _llm_type(self) -> str:
+        """Return type of llm."""
+        return self.service_name + "-" + self.model_name
+
+    def _generate(
+        self,
+        messages: List[BaseMessage],
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+    ) -> ChatResult:
+        llm = CustomLLM(service_name=self.service_name, model_name=self.model_name)
+        prompt = "".join(message.content for message in messages)
+        content = llm(prompt)
+        return ChatResult(generations=[ChatGeneration(message=SystemMessage(content=content))])
+
+
+    async def _agenerate(
+        self,
+        messages: List[BaseMessage],
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[AsyncCallbackManagerForLLMRun] = None,
+    ) -> ChatResult:
+        return ChatResult(generations=[ChatGeneration(message=SystemMessage(content="not used"))])
+
+
+
+
+def testChatModel():
+    chat_model = CustomChatModel(service_name="openai", model_name="gpt-4")
+    message = HumanMessage(content="describe Helsinki in 5 words or less")
+    print(chat_model([message]).content)
+
+
+
+
+
+
+if __name__ == "__main__":
+    #testLLM()
+    testChatModel()
+
